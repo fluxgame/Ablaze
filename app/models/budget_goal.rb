fdb143baa8ecab777abe88db20c0570eeb547cad
David Howand <fluxgame@gmail.com>
2018-05-24 11:05:40 -0400

    add current balance to spending account json
    Former-commit-id: 6c19cb335bea3f41a3cdd2d2206354ae7eb0cdf1

--- app/models/budget_goal.rb
+++ app/models/budget_goal.rb
@@ -4,8 +4,9 @@ class BudgetGoal < ApplicationRecord
   has_many :budgeted_amounts, dependent: :destroy
   
   def remaining_amount
+    return nil if self.new_record?
     Rails.cache.fetch("#{cache_key}/remaining_amount", expires_in: 15.minutes) {
-      BudgetedAmount.where(budget_goal_id: self.id).sum(:amount) - LedgerEntry.where(budget_goal_id: self.id).sum(:debit) - LedgerEntry.where(budget_goal_id: self.id).sum(:credit)
+      (BudgetedAmount.where(budget_goal_id: self.id).sum(:amount) - LedgerEntry.where(budget_goal_id: self.id).sum(:debit) - LedgerEntry.where(budget_goal_id: self.id).sum(:credit)).round(user.home_asset_type.precision)
     }
   end
 end
