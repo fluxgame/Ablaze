d2825ab0b770650fce7b588ea675a556dd30c005
David Howand <fluxgame@gmail.com>
2018-10-04 18:23:31 -0400

    fix tax calculation
    Former-commit-id: 205d53b84730b20b804a3bd78f9552550673eded

--- app/models/user.rb
+++ app/models/user.rb
@@ -203,16 +203,35 @@ class User < ApplicationRecord
     }
   end
   
-  def adjust_for_taxes(annual_spending)
-    federal_tax_rate = 0.1
-    state_tax_rate = 0.051
-  
-    federal_deduction = 24000
-    state_deduction = 8800
-    
-    (annual_spending - federal_deduction * federal_tax_rate - state_deduction * state_tax_rate) / (1 - federal_tax_rate - state_tax_rate)
+  def tax_brackets
+    [[0,8800],[0.051,24000],[0.151,43050],[0.171,101400],[0.271,189000],[0.2951,339000],[0.3751,424000],[0.3951,624000],[0.4251,624000]]
   end
   
+  def adjust_for_taxes(annual_spending)
+    last_bracket_max = 0
+    last_bracket = 0
+    tax_brackets.each_with_index do |bracket,i|
+      bracket_max = last_bracket_max + (1 - bracket[0]) * (bracket[1] - last_bracket)
+      puts bracket_max
+      if annual_spending <= bracket_max || i == tax_brackets.size - 1
+        return last_bracket + (annual_spending - last_bracket_max) / (1 - bracket[0])
+      end
+      last_bracket_max = bracket_max
+      last_bracket = bracket[1]
+    end
+  end
+
+  def calculate_tax(gross_income)
+    tax = 0
+    excluded = 0
+    tax_brackets.each_with_index do |bracket,i|
+      applicable = [bracket[1],gross_income].min - excluded
+      tax += applicable * bracket[0]
+      excluded += applicable
+    end
+    tax
+  end
+    
   def forecast_register
     register = {}
     annual_spending = 0
