36969f29a6ff80a959e597f3c4624d4ea89f5ad6
David Howand <fluxgame@gmail.com>
2018-10-23 08:50:26 -0400

    minor bugfix
    Former-commit-id: b30d2bd120a118f94bf1dc9eab01e7e6a5e56b45

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,14 +3,10 @@ class HomeController < ApplicationController
     @fi_date_data = [{name: "Days to Lean FI", data: []},{name: "Days to Full FI", data: []},{name: "Lean FI Date", data: []},{name: "Full FI Date", data: []}]
     
     current_user.report_data.each do |rd|
-      lean_fi_date = current_user.fi_date(rd.date, current_user.aggregate_amounts(rd.date)[:lean_fi_expenses], rd.net_worth, rd.annual_savings, rd.average_rate_of_return)
       full_fi_date = current_user.fi_date(rd.date, rd.annual_post_fi_spending, rd.net_worth, rd.annual_savings, rd.average_rate_of_return)
       
-      if full_fi_date.present? && lean_fi_date.present?
-        @fi_date_data[0][:data].push([rd.date, lean_fi_date - rd.date])
-        @fi_date_data[1][:data].push([rd.date, full_fi_date - rd.date])
-        @fi_date_data[2][:data].push([rd.date, lean_fi_date.to_time.to_i / (60 * 60 * 24) / 365.25 * 12])
-        @fi_date_data[3][:data].push([rd.date, full_fi_date.to_time.to_i / (60 * 60 * 24) / 365.25 * 12])
+      if full_fi_date.present?
+        @fi_date_data[0][:data].push([rd.date, full_fi_date - rd.date])
       end
     end
       
